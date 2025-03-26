5dcfbb7654cfeb14aacfe9732bd2bbe246a1e57a
CaIzii <2470159730@qq.com>
2025-03-26 03:07:22 -0500

    AIController: introduce AIController.

---
+++ Code/Game/Framework/AIController.cpp
@@ -0,0 +1,20 @@
+﻿#include "AIController.hpp"
+
+AIController::AIController(Map* map): Controller(map)
+{
+}
+
+void AIController::Update(float deltaTime)
+{
+    Controller::Update(deltaTime);
+}
+
+void AIController::Possess(ActorHandle& actorHandle)
+{
+    Controller::Possess(actorHandle);
+}
+
+Actor* AIController::GetActor()
+{
+    return Controller::GetActor();
+}
---
+++ Code/Game/Framework/AIController.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "Controller.hpp"
+
+class AIController : public Controller
+{
+public:
+    AIController(Map* map);
+
+    void   Update(float deltaTime) override;
+    void   Possess(ActorHandle& actorHandle) override;
+    Actor* GetActor() override;
+};
