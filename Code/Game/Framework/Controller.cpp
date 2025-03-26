c51d61346faa618200ba473b2a897278b6445afc
CaIzii <2470159730@qq.com>
2025-03-26 03:07:03 -0500

    Controller: introduce Controller.

---
+++ Code/Game/Framework/Controller.cpp
@@ -0,0 +1,25 @@
+﻿#include "Controller.hpp"
+
+#include "Game/Gameplay/Map.hpp"
+
+
+Controller::Controller(Map* map): m_map(map)
+{
+}
+
+Controller::~Controller()
+{
+}
+
+void Controller::Update(float deltaTime)
+{
+}
+
+void Controller::Possess(ActorHandle& actorHandle)
+{
+}
+
+Actor* Controller::GetActor()
+{
+    return m_map->GetActorByHandle(m_actorHandle);
+}
---
+++ Code/Game/Framework/Controller.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "ActorHandle.hpp"
+
+class Actor;
+class Map;
+
+class Controller
+{
+public:
+    Controller(Map * map);
+    virtual ~Controller();
+
+public:
+    ActorHandle m_actorHandle; // Handle of our currently possessed actor or INVALID if no actor is possessed.
+    Map*        m_map = nullptr; // Reference to the current map for purposes of dereferencing actor handles.
+
+    virtual void Update(float deltaTime);
+    /// Unpossess any currently possessed actor and possess a new one. Notify each actor so it can check for
+    /// restoring AI controllers or handling other changes of possession logic.
+    /// @param actorHandle 
+    virtual void Possess(ActorHandle& actorHandle);
+    /// Returns the currently possessed actor or null if no actor is possessed.
+    virtual Actor* GetActor();
+};
