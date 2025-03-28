340af018c2b6f146a1ea1bba5fd8ccb0acf57929
CaIzii <2470159730@qq.com>
2025-03-28 03:05:21 -0500

    Actor: add fields that correspond with ActorDefinition. - Add func that handle physics. - Add func that handle Controller.

--- Code/Game/Gameplay/Actor.cpp
+++ Code/Game/Gameplay/Actor.cpp
@@ -2,12 +2,18 @@
 
 #include <cstdio>
 
+#include "Weapon.hpp"
+#include "Engine/Core/EngineCommon.hpp"
 #include "Engine/Core/VertexUtils.hpp"
 #include "Engine/Math/FloatRange.hpp"
 #include "Engine/Math/Mat44.hpp"
 #include "Engine/Math/MathUtils.hpp"
 #include "Engine/Renderer/Renderer.hpp"
 #include "Game/GameCommon.hpp"
+#include "Game/Definition/ActorDefinition.hpp"
+#include "Game/Definition/WeaponDefinition.hpp"
+#include "Game/Framework/AIController.hpp"
+#include "Game/Framework/PlayerController.hpp"
 
 Actor::Actor(): m_position(Vec3(0, 0, 0)), m_orientation(EulerAngles(0, 0, 0)), m_color(Rgba8::WHITE), m_bIsStatic(false), m_physicalHeight(2.0f), m_physicalRadius(1.0f)
 {
@@ -24,24 +30,110 @@ Actor::Actor(const Vec3& position, const EulerAngles& orientation, const Rgba8& 
     printf("Object::Actor    + Creating Actor at (%f, %f, %f)\n", m_position.x, m_position.y, m_position.z);
 }
 
+Actor::Actor(const SpawnInfo& spawnInfo)
+{
+    ActorDefinition* definition = ActorDefinition::GetByName(spawnInfo.m_actorName);
+    if (definition == nullptr)
+    {
+        ERROR_AND_DIE(Stringf("Actor::Actor    - Actor definition not found for name \"%s\".\n", spawnInfo.m_actorName.c_str()));
+    }
+    m_definition         = definition;
+    m_physicalHeight     = definition->m_physicsHeight;
+    m_physicalRadius     = definition->m_physicsRadius;
+    m_position           = spawnInfo.m_position;
+    m_orientation        = EulerAngles(spawnInfo.m_orientation);
+    m_collisionZCylinder = ZCylinder(spawnInfo.m_position, m_physicalRadius, m_physicalHeight, true);
+    for (std::string items : definition->m_inventory)
+    {
+        const WeaponDefinition* weapon = WeaponDefinition::GetByName(items);
+        if (weapon)
+            m_weapons.push_back(new Weapon(weapon));
+    }
+    /// AI Controller
+    if (m_definition->m_aiEnabled)
+    {
+        m_aiController = new AIController(m_map);
+        m_aiController->Possess(m_handle);
+    }
+    m_currentWeapon = m_weapons.empty() ? nullptr : m_weapons[0];
+
+    /// Color
+    if (m_definition->m_name == "Marine")
+    {
+        m_color = Rgba8::GREEN;
+    }
+    if (m_definition->m_name == "Demon")
+    {
+        m_color = Rgba8::RED;
+    }
+
+    if (m_definition->m_visible)
+        InitLocalVertex();
+    printf("Object::Actor    + Creating Actor at (%f, %f, %f)\n", m_position.x, m_position.y, m_position.z);
+}
+
+
 Actor::~Actor()
 {
     printf("Object::Actor    - Destroy Actor and free resources\n");
 }
 
+
 void Actor::Update(float deltaSeconds)
 {
+    UNUSED(deltaSeconds)
+    /// Shitty code :D
     m_collisionZCylinder.m_center.x = m_position.x;
     m_collisionZCylinder.m_center.y = m_position.y;
     m_collisionZCylinder.m_center.z = m_position.z + m_physicalHeight / 2.0f;
+    if (m_definition->m_simulated)
+    {
+        UpdatePhysics(deltaSeconds);
+    }
 }
 
-void Actor::SetPosition(Vec3 newPos)
+void Actor::UpdatePhysics(float deltaSeconds)
 {
-    m_position                      = newPos;
-    m_collisionZCylinder.m_center.x = newPos.x;
-    m_collisionZCylinder.m_center.y = newPos.y;
-    m_collisionZCylinder.m_center.z = newPos.z + m_physicalHeight / 2.0f;
+    float dragValue = m_definition->m_drag;
+    Vec3  dragForce = -m_velocity * dragValue;
+    AddForce(dragForce);
+
+    m_velocity += m_acceleration * deltaSeconds;
+    m_position += m_velocity * deltaSeconds;
+
+    if (!m_definition->m_flying)
+    {
+        m_position.z = 0.f;
+    }
+    m_acceleration = Vec3::ZERO;
+}
+
+void Actor::AddForce(Vec3 force)
+{
+    m_acceleration += force;
+}
+
+void Actor::AddImpulse(Vec3 impulse)
+{
+}
+
+void Actor::MoveInDirection(Vec3 direction, float speed)
+{
+    // Normalize the direction to unit vector
+    Vec3 dirNormalized = direction.GetNormalized();
+    // achieve 'speed' as velocity while overcoming drag
+    float dragValue = m_definition->m_drag;
+    Vec3  force     = dirNormalized * (speed * dragValue);
+
+    AddForce(force);
+}
+
+void Actor::TurnInDirection(Vec3 direction)
+{
+    if (m_controller && dynamic_cast<PlayerController*>(m_controller))
+    {
+        m_orientation = EulerAngles(direction);
+    }
 }
 
 ZCylinder& Actor::GetColliderZCylinder()
@@ -63,9 +155,9 @@ void Actor::OnColliedEnter(Actor* other)
         FloatRange rangeB = FloatRange(B_bottom, B_top);
         if (rangeA.IsOverlappingWith(rangeB))
         {
-            Vec2 pos2D        = Vec2(m_position.x, m_position.y);
-            Vec2 otherPos2D   = Vec2(other->m_position.x, other->m_position.y);
-            bool pushed       = PushDiscOutOfDisc2D(otherPos2D, other->m_physicalRadius, pos2D, m_physicalRadius);
+            Vec2 pos2D      = Vec2(m_position.x, m_position.y);
+            Vec2 otherPos2D = Vec2(other->m_position.x, other->m_position.y);
+            PushDiscOutOfDisc2D(otherPos2D, other->m_physicalRadius, pos2D, m_physicalRadius);
             other->m_position = Vec3(otherPos2D.x, otherPos2D.y, other->m_position.z);
         }
         else
@@ -120,8 +212,18 @@ void Actor::OnColliedEnter(AABB3& tileXYZBound)
     }
 }
 
+void Actor::Damage(float damage)
+{
+}
+
 void Actor::Render() const
 {
+    if (m_controller && dynamic_cast<PlayerController*>(m_controller))
+    {
+        PlayerController* playerController = dynamic_cast<PlayerController*>(m_controller);
+        if (!playerController->m_bCameraMode)
+            return;
+    }
     g_theRenderer->SetModelConstants(GetModelToWorldTransform(), m_color);
     g_theRenderer->SetBlendMode(BlendMode::OPAQUE);
     g_theRenderer->BindTexture(m_texture);
@@ -134,16 +236,39 @@ void Actor::Render() const
 
 Mat44 Actor::GetModelToWorldTransform() const
 {
-    Mat44 matTranslation = Mat44::MakeTranslation3D(m_position);
-    matTranslation.Append(m_orientation.GetAsMatrix_IFwd_JLeft_KUp());
+    Mat44       matTranslation = Mat44::MakeTranslation3D(m_position);
+    EulerAngles orientationRender;
+    orientationRender.m_yawDegrees = m_orientation.m_yawDegrees;
+    matTranslation.Append(orientationRender.GetAsMatrix_IFwd_JLeft_KUp());
     return matTranslation;
+}
+
+void Actor::OnPossessed(Controller* controller)
+{
+    m_controller = controller;
+}
+
+void Actor::OnUnpossessed()
+{
+    if (m_aiController)
+    {
+        m_aiController->Possess(m_handle);
+    }
 }
 
 void Actor::InitLocalVertex()
 {
+    if (m_physicalHeight <= 0.001f || m_physicalRadius <= 0.001f)
+    {
+        return;
+    }
     // TODO: consider give a better way to handle base center ZCylinder
     ZCylinder localCylinder = m_collisionZCylinder;
     localCylinder.m_center  = Vec3(0, 0, m_physicalHeight / 2.0f);
     AddVertsForCylinderZ3D(m_vertexes, localCylinder, m_color * 0.5f, AABB2::ZERO_TO_ONE);
     AddVertsForCylinderZ3D(m_vertexesWireframe, localCylinder, m_color, AABB2::ZERO_TO_ONE);
+    Vec3 localConeStartPoint = Vec3(m_definition->m_physicsRadius - 0.05f, 0, m_definition->m_eyeHeight * 0.85f);
+    Vec3 localConeEndPoint   = localConeStartPoint + Vec3(m_definition->m_physicsRadius / 2.f, 0, 0);
+    AddVertsForCone3D(m_vertexes, localConeEndPoint, localConeStartPoint, m_definition->m_physicsHeight / 5.0f, m_color * 0.5f);
+    AddVertsForCone3D(m_vertexesWireframe, localConeEndPoint, localConeStartPoint, m_definition->m_physicsHeight / 5.0f, m_color);
 }
--- Code/Game/Gameplay/Actor.hpp
+++ Code/Game/Gameplay/Actor.hpp
@@ -1,31 +1,63 @@
 ﻿#pragma once
 #include <vector>
 
+#include "Map.hpp"
 #include "Engine/Core/Rgba8.hpp"
 #include "Engine/Core/Vertex_PCU.hpp"
 #include "Engine/Math/EulerAngles.hpp"
 #include "Engine/Math/Vec3.hpp"
+#include "../Definition/MapDefinition.hpp"
 #include "Engine/Math/ZCylinder.hpp"
+#include "Game/Framework/ActorHandle.hpp"
 
 
+class Controller;
+class AIController;
+class Weapon;
+class Map;
+class ActorDefinition;
 class AABB3;
 class AABB2;
 class Texture;
+
 
 class Actor
 {
 public:
     Actor();
+    Actor(const SpawnInfo& spawnInfo);
     Actor(const Vec3& position, const EulerAngles& orientation, const Rgba8& color, float physicalHeight = 2.0f, float physicalRadius = 1.0f, bool bIsStatic = false);
     virtual ~Actor();
 
 public:
-    Vec3        m_position;
-    EulerAngles m_orientation;
-    Rgba8       m_color;
-    bool        m_bIsStatic;
-    float       m_physicalHeight;
-    float       m_physicalRadius;
+    Vec3        m_position; // 3D position, as a Vec3, in world units.
+    EulerAngles m_orientation; // 3D orientation, as EulerAngles, in degrees.
+    Vec3        m_velocity; // 3D velocity, as a Vec3, in world units per second.
+    Vec3        m_acceleration; // 3D acceleration, as a Vec3, in world units per second squared.
+
+    bool  m_bIsStatic;
+    float m_physicalHeight;
+    float m_physicalRadius;
+
+    Rgba8 m_color;
+
+    ActorDefinition* m_definition = nullptr; // A reference to our actor definition.
+    Map*             m_map        = nullptr; // Reference to the map that spawned us.
+    ActorHandle      m_handle     = ActorHandle::INVALID; // This actor's handle, assigned by the map when this actor was spawned.
+    float            m_health     = 1.f; // Current health.
+    Actor*           m_owner      = nullptr; //Only applies to projectile actors. Actor that fired this projectile, for purposes of collision filtering.
+    float            m_dead       = 0.f; // Any data needed to track if and how long we have been dead.
+    bool             m_bIsGarbage = false; // If true, this actor is no longer needed and can be safely deleted.
+
+    // A reference to our default AI controller, if any. Used to keep track of our AI controller
+    // if the player possesses this actor, in which case he pushes the AI out of the way until
+    // he releases possession.
+    AIController* m_aiController = nullptr;
+    // A reference to the controller currently possessing us, if any.
+    Controller* m_controller = nullptr;
+
+    std::vector<Weapon*> m_weapons;
+    Weapon*              m_currentWeapon = nullptr;
 
 private:
     ZCylinder               m_collisionZCylinder;
@@ -34,8 +66,29 @@ private:
     Texture*                m_texture = nullptr;
 
 public:
-    void       Update(float deltaSeconds);
-    void       SetPosition(Vec3 newPos);
+    void Update(float deltaSeconds);
+    /// Perform physics processing if the actor is simulated. Set velocity Z-components to zero for non-flying actors.
+    /// Add a drag force equal to our drag times our negative current velocity. Integrate acceleration, velocity,
+    /// and position then clear out acceleration for next frame.
+    /// @param deltaSeconds 
+    void UpdatePhysics(float deltaSeconds);
+    /// Add a force to our acceleration to be used next frame. Must be called every frame to apply continual force over time.
+    /// @param force 
+    void AddForce(Vec3 force);
+    /// Add a force to our acceleration to be used next frame. Must be called every frame to apply continual force over time.
+    /// @param impulse 
+    void AddImpulse(Vec3 impulse);
+    /// Move in a specified direction at a specified speed. Movement will be done by physics simulation so
+    /// add a force to make the actor move. Force should be equal in magnitude to the speed times our drag,
+    /// to give us enough acceleration to overcome drag. Caller is trusted to set a speed within our limits.
+    /// @param direction 
+    /// @param speed 
+    void MoveInDirection(Vec3 direction, float speed);
+    /// Turn in the shortest possible path in the supplied direction, up to a maximum that must also be provided.
+    /// Set the actor orientation directly rather than using angular velocity or other physics simulation.
+    /// The caller is trusted to determine the maximum amount to turn based on turn rate and delta seconds. 
+    /// @param direction 
+    void       TurnInDirection(Vec3 direction);
     ZCylinder& GetColliderZCylinder();
     /// Handle Actor collied with other actors
     /// @param other 
@@ -45,9 +98,17 @@ public:
     void OnColliedEnter(AABB2& tileXYBound);
     /// Handle Actor collied with Tile bound in Z axis
     /// @param tileXYZBound 
-    void  OnColliedEnter(AABB3& tileXYZBound);
+    void OnColliedEnter(AABB3& tileXYZBound);
+    /// Take damage, handle dying due to health dropping below zero, and notify our controller of the source.
+    /// @param damage 
+    void Damage(float damage);
+
     void  Render() const;
     Mat44 GetModelToWorldTransform() const;
+
+    /// AI
+    void OnPossessed(Controller* controller); // Callback for actors when possessed by a controller.
+    void OnUnpossessed(); // Callback for actors when unpossessed by a controller. 
 
 private:
     void InitLocalVertex();
