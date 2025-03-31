caadb8d3fa88e21f6f76751471037eabcf229886
CaIzii <2470159730@qq.com>
2025-03-30 22:07:06 -0500

    AIController: implement AIC func `Update()`.

--- Code/Game/Framework/AIController.cpp
+++ Code/Game/Framework/AIController.cpp
@@ -1,5 +1,8 @@
 ﻿#include "AIController.hpp"
 
+#include "Game/Definition/ActorDefinition.hpp"
+#include "Game/Gameplay/Map.hpp"
+
 AIController::AIController(Map* map): Controller(map)
 {
 }
@@ -7,6 +10,13 @@ AIController::AIController(Map* map): Controller(map)
 void AIController::Update(float deltaTime)
 {
     Controller::Update(deltaTime);
+    Actor* controlledActor = m_map->GetActorByHandle(m_actorHandle);
+    Actor* target          = m_map->GetClosestVisibleEnemy(controlledActor);
+    if (target && m_targetActorHandle.IsValid() && m_targetActorHandle != target->m_handle)
+    {
+        m_targetActorHandle = target->m_handle; // switch
+        printf("AIController::Update    > Target actor changed to %s\n", target->m_definition->m_name.c_str());
+    }
 }
 
 void AIController::Possess(ActorHandle& actorHandle)
@@ -22,4 +32,5 @@ Actor* AIController::GetActor()
 void AIController::DamagedBy(ActorHandle& attacker)
 {
     m_targetActorHandle = attacker;
+    printf("AIController::Update    > Target actor changed to %s\n", m_map->GetActorByHandle(attacker)->m_definition->m_name.c_str());
 }
