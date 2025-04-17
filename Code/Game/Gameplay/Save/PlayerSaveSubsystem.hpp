18eff48afba58b3086657c90a0dbb2bf57e8bedb
CaIzii <2470159730@qq.com>
2025-04-17 02:50:59 -0500

    PlayerSaveSubsystem:  introduce `PlayerSaveSubsytem` that owns, modify player save data and slot for gold features.

---
+++ Code/Game/Gameplay/Save/PlayerSaveSubsystem.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <vector>
+
+struct PlayerSaveData
+{
+    int m_playerID    = -1;
+    int m_numOfDeaths = 0;
+    int m_numOfKilled = 0;
+};
+
+class PlayerSaveSubsystem
+{
+private:
+    static std::vector<PlayerSaveData> s_playerSavedData;
+
+public:
+    static void            ClearSaves();
+    static PlayerSaveData* GetPlayerSaveData(int id);
+    static bool CreatePlayerSaveData(PlayerSaveData newSaveData);
+    static bool            DoesPlayerSaveDataExist(int id);
+};
